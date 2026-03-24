6a9420cd97e0859f1f09eeae7c3c06c98e580dd1
Rohit Kakati <rohitdynamics@gmail.com>
2026-03-24 21:17:19 +0530

    website completed

--- eas.js
+++ eas.js
@@ -10,27 +10,90 @@ const artboard = document.querySelector('#artboard');
 // declaring global variables
 let arrColumn = [];
 let arrRow = [];
-let pattern;
 let opacity = 0;
-let btnactive = false;
+let patternbtnactive = false;
+let startbtnactive = false;
 
-// declaring function to create grid
-let startart = () => {
+// start to draw function
+function startart(){
+    if(gridinput.value<100 && patternbtnactive == true && startbtnactive == false){
+        startbtn.style.backgroundColor = 'yellow';
+        makeGrid();
+    } else if(gridinput.value<100 && patternbtnactive == true && startbtnactive == true) {
+        alert('To start again please click Restart button')
+    } else if(gridinput.value>=100){
+        alert('Please enter grid size less than 100')
+    } else {
+        alert('To start drawing, please:\n1)Enter grid size.\n2)Select colour pattern.')
+    }  
+}
 
-    if(gridinput.value && btnactive == true){
-        startbtn.style.backgroundColor = 'white';
-        for (let i=0; i<gridinput.value; i++){
-            arrColumn[i] = document.createElement('div');
-            artboard.appendChild(arrColumn[i]);
-            arrColumn[i].setAttribute("style", "flex: 1; display: flex; flex-direction: column");
+//function to remove 'active' class to avoid future conflict
+let removeButtonActiveStates = () => {
+    rgbbtn.classList.remove('active');
+    shadebtn.classList.remove('active');
+    blackbtn.classList.remove('active');
+}
 
-            for (let j=0; j<gridinput.value; j++){
-            arrRow[j] = document.createElement('div');
-            arrColumn[i].appendChild(arrRow[j]);
-            arrRow[j].setAttribute("style", "flex: 1;");
-            arrRow[j].addEventListener("mouseenter", (event) => {
+//function to change colour of active button
+let changeActiveButtonColor = () => {
+    if(rgbbtn.classList.contains('active')){
+        rgbbtn.style.backgroundColor = 'yellow';
+        blackbtn.style.backgroundColor = '';
+        shadebtn.style.backgroundColor = '';
+        } else if (blackbtn.classList.contains('active')){
+        blackbtn.style.backgroundColor = 'yellow';
+        rgbbtn.style.backgroundColor = '';
+        shadebtn.style.backgroundColor = '';          
+        } else if (shadebtn.classList.contains('active')){
+        shadebtn.style.backgroundColor = 'yellow';
+        blackbtn.style.backgroundColor = '';
+        rgbbtn.style.backgroundColor = '';
+    }
+}
 
-                if(rgbbtn.classList.contains('active')){
+// event listeners for buttons
+startbtn.addEventListener("click", () => {
+    startart();
+    startbtnactive = true;
+});
+rgbbtn.addEventListener("click", () => {
+    patternbtnactive = true;
+    rgbbtn.classList.add('active')
+    blackbtn.classList.remove('active');
+    shadebtn.classList.remove('active');
+    changeActiveButtonColor();
+});
+blackbtn.addEventListener("click", () => {
+    patternbtnactive = true;
+    rgbbtn.classList.remove('active')
+    blackbtn.classList.add('active');
+    shadebtn.classList.remove('active');
+    changeActiveButtonColor();
+});
+shadebtn.addEventListener("click", () => {
+    patternbtnactive = true;
+    rgbbtn.classList.remove('active')
+    blackbtn.classList.remove('active');
+    shadebtn.classList.add('active');
+    opacity = 0;
+    changeActiveButtonColor();
+});
+resetbtn.addEventListener("click", () => {
+    opacity = 0;
+    patternbtnactive = false;
+    startbtnactive = false;
+    removeButtonActiveStates();
+    startbtn.style.backgroundColor = '';
+    rgbbtn.style.backgroundColor = '';
+    shadebtn.style.backgroundColor = '';
+    blackbtn.style.backgroundColor = '';
+    artboard.innerHTML = "";
+    gridinput.value = ""; 
+})
+
+function patternSelection(){
+    if(rgbbtn.classList.contains('active')){
                     let r = Math.floor(Math.random()*256);
                     let g = Math.floor(Math.random()*256);
                     let b = Math.floor(Math.random()*256);
@@ -45,71 +108,22 @@ let startart = () => {
                     opacity = opacity + 0.1;
                 }
 
-                event.currentTarget.style.backgroundColor = pattern;
-                console.log('Mouse hovered over me')
+                return pattern;
+};
+
+function makeGrid(){
+    for (let i=0; i<gridinput.value; i++){
+            arrColumn[i] = document.createElement('div');
+            artboard.appendChild(arrColumn[i]);
+            arrColumn[i].setAttribute("style", "flex: 1; display: flex; flex-direction: column");
+
+            for (let j=0; j<gridinput.value; j++){
+                arrRow[j] = document.createElement('div');
+                arrColumn[i].appendChild(arrRow[j]);
+                arrRow[j].setAttribute("style", "flex: 1;");
+                arrRow[j].addEventListener("mouseenter", (event) => {
+                event.currentTarget.style.backgroundColor = patternSelection();
                });
             };
-        };    
-    } else {
-        alert('To start drawing, please:\n1)Enter grid size.\n2)Select colour pattern.')
-    }    
-}
-
-//function to remove 'active' class to avoid future conflict
-let removeButtonActiveStates = () => {
-    rgbbtn.classList.remove('active');
-    shadebtn.classList.remove('active');
-    blackbtn.classList.remove('active');
-}
-
-//function to change colour of active button
-let changeActiveButtonColor = () => {
-    if(rgbbtn.classList.contains('active')){
-        rgbbtn.style.backgroundColor = 'white';
-        blackbtn.style.backgroundColor = '';
-        shadebtn.style.backgroundColor = '';
-        } else if (blackbtn.classList.contains('active')){
-        blackbtn.style.backgroundColor = 'white';
-        rgbbtn.style.backgroundColor = '';
-        shadebtn.style.backgroundColor = '';          
-        } else if (shadebtn.classList.contains('active')){
-        shadebtn.style.backgroundColor = 'white';
-        blackbtn.style.backgroundColor = '';
-        rgbbtn.style.backgroundColor = '';
     }
-}
-
-startbtn.addEventListener("click", startart);
-
-rgbbtn.addEventListener("click", () => {
-    btnactive = true;
-    rgbbtn.classList.add('active')
-    blackbtn.classList.remove('active');
-    shadebtn.classList.remove('active');
-    changeActiveButtonColor();
-});
-blackbtn.addEventListener("click", () => {
-    btnactive = true;
-    rgbbtn.classList.remove('active')
-    blackbtn.classList.add('active');
-    shadebtn.classList.remove('active');
-    changeActiveButtonColor();
-});
-shadebtn.addEventListener("click", () => {
-    btnactive = true;
-    rgbbtn.classList.remove('active')
-    blackbtn.classList.remove('active');
-    shadebtn.classList.add('active');
-    opacity = 0;
-    changeActiveButtonColor();
-});
-resetbtn.addEventListener("click", () => {
-    opacity = 0;
-    btnactive = false;
-    removeButtonActiveStates();
-    startbtn.style.backgroundColor = '';
-    for (let i=0; i<gridinput.value; i++){
-            artboard.removeChild(arrColumn[i]);
-    }
-    gridinput.value = ""; 
-})
+};    
